8fe5a5101e63ce9bb1f00a3f2b5eede3c6b60f3b
Daniel Teske <daniel.teske@europace.de>
2020-02-12 00:18:11 +0100

    use esc countries geojson

--- src/app.js
+++ src/app.js
@@ -1,10 +1,10 @@
 import * as d3 from 'd3'
-import * as topojson from 'topojson'
+import proj4 from 'proj4'
 
 let mapData = null;
 
 (async function () {
-  mapData = await d3.json("/data/lim.json");
+  mapData = await d3.json("/data/esc-countries-wgs84.geojson");
   drawCharts();
   window.addEventListener("resize", drawCharts);
 })();
@@ -25,16 +25,10 @@ function drawChart(targetElement) {
   const innerWidth = targetElement.node().clientWidth,
     innerHeight = targetElement.node().clientHeight;
 
+  fitProjection(mapData, innerWidth, innerHeight);
+
   const width = innerWidth;
   const height = innerHeight;
-
-  const projection = d3.geoMercator()
-    .scale(5000)
-    .center([1.8, 41.9])
-    .translate([width / 2, height / 2]);
-
-  const path = d3.geoPath()
-    .projection(projection);
 
   const svg = targetElement.append("svg")
     .attr("width", width)
@@ -42,8 +36,18 @@ function drawChart(targetElement) {
 
   const g = svg.append("g")
     .on("wheel.zoom", function () {
+      console.log("event",event);
       var currScale = projection.scale();
-      var newScale = currScale - 2 * event.deltaY;
+      console.log("currScale",currScale);
+      var newScale = currScale - 0.000005 * event.deltaY;
+      if(event.deltaY < 0){
+        console.log("in");
+        newScale = Math.min(0.001, newScale);
+      }else{
+        console.log("out");
+        newScale = Math.max(0.00006, newScale);
+      }
+      console.log("newScale",newScale);
       var currTranslate = projection.translate();
       var coords = projection.invert([event.offsetX, event.offsetY]);
       projection.scale(newScale);
@@ -64,13 +68,55 @@ function drawChart(targetElement) {
     .attr("class", "background")
     .attr("width", width)
     .attr("height", height)
-    .style("fill", "#fcf4e0");
+    .style("fill", "#0077be");
 
   g.selectAll(".com")
-    .data(topojson.feature(mapData, mapData.objects.limits).features)
+    .data(mapData.features)
     .enter()
     .append("path")
     .attr("id", "state-borders")
-    .attr("d", path);
+    .attr("d", path)
+    .style("fill", "#4a6170")
+    .style("stroke", "#fff");
 
+}
+
+//
+// Projection Stuff
+//
+const epsg32632 = proj4("PROJCS[\"WGS 84 / UTM zone 32N\",GEOGCS[\"WGS 84\",DATUM[\"WGS_1984\",SPHEROID[\"WGS 84\",6378137,298.257223563,AUTHORITY[\"EPSG\",\"7030\"]],AUTHORITY[\"EPSG\",\"6326\"]],PRIMEM[\"Greenwich\",0,AUTHORITY[\"EPSG\",\"8901\"]],UNIT[\"degree\",0.01745329251994328,AUTHORITY[\"EPSG\",\"9122\"]],AUTHORITY[\"EPSG\",\"4326\"]],UNIT[\"metre\",1,AUTHORITY[\"EPSG\",\"9001\"]],PROJECTION[\"Transverse_Mercator\"],PARAMETER[\"latitude_of_origin\",0],PARAMETER[\"central_meridian\",9],PARAMETER[\"scale_factor\",0.9996],PARAMETER[\"false_easting\",500000],PARAMETER[\"false_northing\",0],AUTHORITY[\"EPSG\",\"32632\"],AXIS[\"Easting\",EAST],AXIS[\"Northing\",NORTH]]");
+
+const project = function(lambda, phi) {
+  return epsg32632
+    .forward([lambda, phi].map(radiansToDegrees));
+};
+
+project.invert = function(x, y) {
+  return epsg32632
+    .inverse([x, y]).map(degreesToRadians);
+};
+
+const projection = d3.geoProjection(project)
+  .scale(500)
+  .center([12.0, 53.0])
+  .translate([500 / 2, 500 / 2]);
+
+const path = d3.geoPath()
+  .projection(projection);
+
+function degreesToRadians(degrees) { return degrees * Math.PI / 180; }
+function radiansToDegrees(radians) { return radians * 180 / Math.PI; }
+
+function fitProjection(geometry, width, height) {
+  projection
+    .scale(1)
+    .translate([0, 0]);
+
+  const b = path.bounds(geometry),
+    s = .95 / Math.max((b[1][0] - b[0][0]) / width, (b[1][1] - b[0][1]) / height),
+    t = [(width - s * (b[1][0] + b[0][0])) / 2, (height - s * (b[1][1] + b[0][1])) / 2];
+
+  projection
+    .scale(s)
+    .translate(t);
 }
