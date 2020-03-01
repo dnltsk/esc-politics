8a3725fdce92c5ecf4b65cdf24e68fbea4d27614
Daniel Teske <daniel.teske@europace.de>
2020-03-01 23:23:12 +0100

    true Map class

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,13 @@
 import * as d3 from 'd3';
-import {drawChart} from "./map";
+import {Map} from "./map";
 import {FeatureCollection, Polygon} from "geojson";
 import {CountryProperties} from "./types";
 
 export class App {
 
   mapData: FeatureCollection<Polygon, CountryProperties> = null;
+
+  map = new Map();
 
   constructor() {
     d3.json("data/esc-countries.geojson").then((data) => {
@@ -16,10 +18,10 @@ export class App {
   }
 
   public drawCharts() {
-    drawChart(d3.select(".ul"), this.mapData);
-    drawChart(d3.select(".ur"), this.mapData);
-    drawChart(d3.select(".ll"), this.mapData);
-    drawChart(d3.select(".lr"), this.mapData);
+    this.map.drawChart(d3.select(".ul"), this.mapData);
+    this.map.drawChart(d3.select(".ur"), this.mapData);
+    this.map.drawChart(d3.select(".ll"), this.mapData);
+    this.map.drawChart(d3.select(".lr"), this.mapData);
   }
 
 }
--- src/map.ts
+++ src/map.ts
@@ -3,77 +3,87 @@ import * as d3 from "d3";
 import {FeatureCollection, Polygon} from "geojson";
 import {CountryProperties} from "./types";
 
-const projection = d3.geoProjection(project)
-  .scale(500)
-  .center([12.0, 53.0])
-  .translate([500 / 2, 500 / 2]);
+export class Map {
 
-const path = d3.geoPath()
-  .projection(projection);
+  readonly projection = d3.geoProjection(project)
+    .scale(500)
+    .center([12.0, 53.0])
+    .translate([500 / 2, 500 / 2]);
 
-export function drawChart(targetElement: d3.Selection<HTMLElement, {}, HTMLElement, any>, mapData: FeatureCollection<Polygon, CountryProperties>) {
-  console.log("drawChart", mapData);
-  targetElement.html("");
+  readonly path = d3.geoPath()
+    .projection(this.projection);
 
-  const innerWidth = targetElement.node().clientWidth,
-    innerHeight = targetElement.node().clientHeight;
-
-  fitToProjection(path, projection, mapData, innerWidth, innerHeight);
-
-  const width = innerWidth;
-  const height = innerHeight;
-
-  const svg = targetElement.append("svg")
-    .attr("width", width)
-    .attr("height", height);
-
-  const g = svg.append("g");
-  g.call(d3.drag().on("drag", panFunction));
-  g.call(d3.zoom()).on("wheel.zoom", zoomFunction);
-
-  g.append("rect")
-    .attr("class", "background")
-    .attr("width", width)
-    .attr("height", height)
-    .style("fill", "#0077be");
-
-  g.selectAll(".com")
-    .data(mapData.features)
-    .enter()
-    .append("path")
-    .attr("id", "state-borders")
-    .attr("d", path)
-    .style("fill", "#4a6170")
-    .style("stroke", "#fff");
-
-}
-
-function zoomFunction() {
-  let g = d3.select(this);
-  const ZOOM_FACTOR = 0.000005;
-  const ZOOM_IN_LIMIT = 0.001;
-  const ZOOM_OUT_LIMIT = 0.00006;
-  const currScale = projection.scale();
-  let newScale = currScale - ZOOM_FACTOR * d3.event.deltaY;
-  if (d3.event.deltaY < 0) {
-    newScale = Math.min(ZOOM_IN_LIMIT, newScale);
+  constructor() {
   }
-  else {
-    newScale = Math.max(ZOOM_OUT_LIMIT, newScale);
+
+  public drawChart(targetElement: d3.Selection<HTMLElement, {}, HTMLElement, any>, mapData: FeatureCollection<Polygon, CountryProperties>) {
+    targetElement.html("");
+
+    const innerWidth = targetElement.node().clientWidth,
+      innerHeight = targetElement.node().clientHeight;
+
+    fitToProjection(this.path, this.projection, mapData, innerWidth, innerHeight);
+
+    const width = innerWidth;
+    const height = innerHeight;
+
+    const svg = targetElement.append("svg")
+      .attr("width", width)
+      .attr("height", height);
+
+    const g = svg.append("g");
+
+    g.call(d3.drag().on("drag",
+      (d, i, n) => this.dragFunction(d3.select(n[i])))
+    );
+
+    g.call(d3.zoom()).on("wheel.zoom",
+      (d, i, n) => this.zoomFunction(d3.select(n[i]))
+    );
+
+    g.append("rect")
+      .attr("class", "background")
+      .attr("width", width)
+      .attr("height", height)
+      .style("fill", "#0077be");
+
+    g.selectAll(".com")
+      .data(mapData.features)
+      .enter()
+      .append("path")
+      .attr("id", "state-borders")
+      .attr("d", this.path)
+      .style("fill", "#4a6170")
+      .style("stroke", "#fff");
+
   }
-  const currTranslate = projection.translate();
-  const coords = projection.invert([d3.event.offsetX, d3.event.offsetY]);
-  projection.scale(newScale);
-  const newPos = projection(coords);
 
-  projection.translate([currTranslate[0] + (d3.event.offsetX - newPos[0]), currTranslate[1] + (d3.event.offsetY - newPos[1])]);
-  g.selectAll("path").attr("d", path);
-}
+  private zoomFunction(g: d3.Selection<SVGElement, {}, HTMLElement, any>) {
+    const ZOOM_FACTOR = 0.000005;
+    const ZOOM_IN_LIMIT = 0.001;
+    const ZOOM_OUT_LIMIT = 0.00006;
+    const currScale = this.projection.scale();
+    let newScale = currScale - ZOOM_FACTOR * d3.event.deltaY;
+    if (d3.event.deltaY < 0) {
+      newScale = Math.min(ZOOM_IN_LIMIT, newScale);
+    }
+    else {
+      newScale = Math.max(ZOOM_OUT_LIMIT, newScale);
+    }
+    const currTranslate = this.projection.translate();
+    const coords = this.projection.invert([d3.event.offsetX, d3.event.offsetY]);
+    this.projection.scale(newScale);
+    const newPos = this.projection(coords);
 
-function panFunction() {
-  let g = d3.select(this);
-  const currTranslate = projection.translate();
-  projection.translate([currTranslate[0] + d3.event.dx,
-                        currTranslate[1] + d3.event.dy]);
-  g.selectAll("path").attr("d", path);
+    this.projection.translate([currTranslate[0] + (d3.event.offsetX - newPos[0]), currTranslate[1] + (d3.event.offsetY - newPos[1])]);
+    g.selectAll("path").attr("d", this.path);
+  }
+
+  private dragFunction(g: d3.Selection<Element, {}, HTMLElement, any>) {
+    const currTranslate = this.projection.translate();
+    this.projection.translate([currTranslate[0] + d3.event.dx,
+                               currTranslate[1] + d3.event.dy]);
+    g.selectAll("path").attr("d", this.path);
+  }
+
 }
